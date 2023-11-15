af1a80fc73b2cc9cf0cc30d0d564e93cfcc800cf
SimonSok96 <Sokolov.sam@gmail.com>
2023-11-15 20:01:28 +0200

    Homework1

---
+++ mysite/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'created']
+    list_filter = ['created', 'created', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    ordering = ['created',]
+
---
+++ mysite/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
