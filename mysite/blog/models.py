bec5e25451cd83bcfec4ed88569a2b1744d31bfd
SimonSok96 <Sokolov.sam@gmail.com>
2023-11-19 20:27:15 +0200

    Homework1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(created__year=2023)
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post found')
+
+    return render(request, 'blog/detail.html', {'post': post})
