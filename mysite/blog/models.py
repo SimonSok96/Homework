ee00f0e8621dec9906cba4878ba573629b9a1143
SimonSok96 <Sokolov.sam@gmail.com>
2023-11-22 19:39:19 +0200

    22.11.23 pagination

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -9,9 +10,10 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=250, unique_for_date='created')
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
@@ -19,3 +21,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.created.year,
+                                                          self.created.month,
+                                                          self.created.day,
+                                                          self.slug,])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,34 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Посторінкове розбиття з 3 постами на сторінку
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, 'blog/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No Post found')
+# def post_detail(request, id):
+#     try:
+#         post = Post.objects.get(id=id)
+#     except Post.DoesNotExist:
+#         raise Http404('No Post found')
+#
+#     return render(request, 'blog/detail.html', {'post': post})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post)
 
     return render(request, 'blog/detail.html', {'post': post})
