0ee8423f8f014177cd67e6991f1ff91d506d6e01
SimonSok96 <Sokolov.sam@gmail.com>
2023-12-27 23:01:58 +0200

    Sitemap

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 
@@ -10,7 +11,6 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     slug = models.SlugField(max_length=250, unique_for_date='created')
     title = models.CharField(max_length=250)
@@ -18,7 +18,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
     published = PublishedManager()
-
+    tags = TaggableManager()
+    
     def __str__(self):
         return self.title
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,17 +3,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from taggit.models import Tag
+from django.db.models import Count
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
 
-class PostListView(ListView):
-    """Альтернативне подання списку постів"""
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/list.html'
-    
+# class PostListView(ListView):
+#     """Альтернативне подання списку постів"""
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/list.html'
+
     
 def post_share(request, post_id):
     # Отримати пост за id
@@ -37,18 +39,22 @@ def post_share(request, post_id):
     return render(request, 'blog/share.html', {'post': post, 'form': form, 'sent': sent})
     
     
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Посторінкове розбиття з 3 постами на сторінку
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     return render(request, 'blog/list.html', {'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+     # Посторінкове розбиття з 3 постами на сторінку
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        post_list = paginator.page(page_number)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    return render(request, "blog/list.html", {"posts": post_list, "tag": tag})
 
 
 # def post_detail(request, id):
@@ -66,7 +72,12 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для коментування користувачами
     form = CommentForm()
-    return render(request, "blog/detail.html", {"post": post, 'comments': comments, 'form': form})
+    # Список подібних постів
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-created')[:4]
+    return render(request, "blog/detail.html", 
+                  {"post": post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 @require_POST
